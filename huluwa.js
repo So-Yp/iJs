4baa5a2a75e41b77256e40335ce65e278c241121
So-Yp <729522609@qq.com>
2024-03-26 17:56:53 +0800

    Update huluwa.js

--- huluwa.js
+++ huluwa.js
@@ -132,14 +132,14 @@ let sendMessage = [];
         $.done();
  });
 function setdata(headers,accessToken,userAgent,cookie,name) {
-    
+    console.log(`${cookie}🎉\n${name}🎉\n`);
     $.setdata(
         JSON.stringify({
             headers: headers,
             accessToken,
             userAgent,
         }),
-        `${cookie}`
+        cookie
     )
     console.log(`获取${name}数据成功🎉\n Token:${accessToken}\n User-Agent:${userAgent}🎉`);
     Message = `获取${name}数据成功🎉\n Token:${accessToken}\n User-Agent:${userAgent}🎉`
